4ef21ec55ff0bff4b024bf69ea30fc9733dbe3e9
alekla0126 <alekla0126@gmail.com>
2023-06-28 19:49:21 +0300

    V1.0.1

---
+++ delivery/lib/delivery_home.dart
@@ -0,0 +1,94 @@
+import 'package:fast_cached_network_image/fast_cached_network_image.dart';
+import 'category_service/category_provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'home_bloc/delivery_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key});
+
+  static const TextStyle _categoryTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<DeliveryBloc>(
+      create: (context) => DeliveryBloc(categoryDataProvider: CategoryDataProvider()),
+      child: BlocConsumer<DeliveryBloc, DeliveryState>(
+        listener: (context, state) {
+          if (state is DeliveryErrorState) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.error)),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is DeliveryInitialState) {
+            BlocProvider.of<DeliveryBloc>(context).add(FetchCategoriesEvent());
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is DeliveryLoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is DeliveryLoadedState) {
+            return Center(
+              child: Container(
+                width: 400.0, // Set the maximum width of the cards
+                child: ListView.builder(
+                  itemCount: state.categories.length,
+                  itemBuilder: (context, index) {
+                    final category = state.categories[index];
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Stack(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Image.network(
+                              category.imageUrl,
+                              fit: BoxFit.cover,
+                              loadingBuilder: (context, child, loadingProgress) {
+                                if (loadingProgress == null) return child;
+                                return CircularProgressIndicator(
+                                  color: Colors.red,
+                                  value: loadingProgress.cumulativeBytesLoaded /
+                                      loadingProgress.expectedTotalBytes!,
+                                );
+                              },
+                              errorBuilder: (context, error, stackTrace) {
+                                return const Icon(Icons.error);
+                              },
+                            ),
+                          ),
+                          Positioned(
+                            top: 8.0,
+                            left: 8.0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                category.name,
+                                style: _categoryTextStyle,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          } else {
+            // If somehow none of the above states match
+            return const Center(child: Text('Unknown state'));
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ delivery/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'delivery_home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Delivery App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DeliveryHome(),
+    );
+  }
+}
+
+class DeliveryHome extends StatefulWidget {
+  const DeliveryHome() : super();
+  @override
+  _DeliveryHomeState createState() => _DeliveryHomeState();
+}
+
+class _DeliveryHomeState extends State<DeliveryHome> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    const HomeScreen(),
+    // SearchScreen(),
+    // ShoppingCartScreen(),
+    // AccountScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final currentDate = DateFormat.yMMMMd().format(DateTime.now()); // Get current date
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const Icon(Icons.location_on, color: Colors.black),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Location",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  currentDate, // Display current date
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+            CircleAvatar(),
+          ],
+        ),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Shopping Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "Account",
+          ),
+        ],
+      ),
+    );
+  }
+}
